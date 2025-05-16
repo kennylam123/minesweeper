b47b0de6edaef0a385df203ad97544dbf675c586
kenny lam <kenny_lam555@hotmail.com>
2025-05-17 00:49:24 +0200

    gui for game

--- main.py
+++ main.py
@@ -1,1 +1,17 @@
-print("hello world")
+from tkinter import *
+import settings
+
+root = Tk()
+root.configure(bg="black")
+root.title('Minesweeper')
+root.geometry(f'{settings.WIDTH}x{settings.HEIGHT}')
+w = Label(root, text='MineSweeper')
+w.pack()
+
+root.resizable(False,False)
+
+
+
+button=Button(root, text='Exit', width=25, command=root.destroy)
+button.pack()
+root.mainloop()
