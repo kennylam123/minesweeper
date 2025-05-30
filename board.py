5ec54b4cdeb9acaf3af8f57f34ebbceb1c4befed
kenny lam <kenny_lam555@hotmail.com>
2025-05-30 14:17:21 +0200

    added flagging cells

--- board.py
+++ board.py
@@ -18,3 +18,17 @@ class Board:
                 cell.button.grid(row=y, column=x)  # place button in grid
                 row.append(cell)
             self.grid.append(row)
+
+    def setAdjMines(self):
+        for row in self.grid:
+          for cell in row:
+             if cell.is_mine:
+                continue
+             count=0
+             for neighbor in self.get_neighbors(cell):
+                if neighbor.is_mine:
+                   count+=1
+                   cell.neighborMines=count
+
+    
+             
--- cells.py
+++ cells.py
@@ -6,30 +6,53 @@ class Cell:
         self.is_mine=is_mine
         self.is_flagged=False
         self.is_revealed=False
+        self.neighborMines=0
         
         self.button=create_button(
             parent=parent_frame,
             width=2,
             height=1,
-            command=self.on_click
+            
 
     )
+        
+        self.button.bind("<Button-1>", self.on_left_click)
+        self.button.bind("<Button-3>", self.on_right_click)
+
     
 
-    def on_click(self): 
-        if self.is_flagged or self.is_revealed:
+    def on_left_click(self,event=None): 
+       if self.is_flagged or self.is_revealed:
+           return
+       self.reveal()
+       print('leftclicked')
+
+    def on_right_click(self, event=None):
+        if self.is_revealed:
             return
-        if self.is_mine: #lost game
-            print('game over')
+        self.is_flagged= not self.is_flagged
+        if self.is_flagged:
+            self.button.config(text='F', fg='red')
+            print('flagged')
+        else:
+            self.button.config(text='', fg='lightgray')
+            print('unflagged')
+
 
 
     def reveal(self):
         self.is_revealed=True
-        self.button.config(
-            text='',
-            bg='gray'
-        )
+        if self.neighborMines>0:
+            self.button.config(text=str(self.neighborMines))
+        else:
+             self.button.config(bg='lightgray')
 
-
-
-        
+        if self.is_flagged or self.is_revealed:
+                return
+                
+        if self.is_mine: #lost game
+             self.button.config(
+                    
+            print('game over')    
+             )
+     
